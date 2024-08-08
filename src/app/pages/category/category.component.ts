7f86cbf92497fdfbfccfa65ab0bbc3582af9c63b
phuongvo1110 <vptphuong21@clc.fitus.edu.vn>
2024-08-08 11:53:01 +0700

    fixing UI

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -9,36 +9,32 @@ export class CategoryComponent {
   modalOpenForm = false;
   secondModalOpenForm = false; // New state for second modal
   modalTitleForm = "";
-  secondModalTitleForm = "Confirmation Required"; // Title for second modal
+  secondModalTitleForm = "Send gift to your friend"; // Title for second modal
   firstModalContent = "This is the gift from AAA event";
-  secondModalContent = "This is the content of the second modal.";
   imgUrl = "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg";
 
   openModal() {
-    console.log('Opening First Modal');
     this.modalTitleForm = "Item A";
     this.modalOpenForm = true;
   }
 
   onClose() {
-    console.log('Closing First Modal');
     this.modalOpenForm = false;
+    // this.secondModalOpenForm = false;
+  }
+  onCloseSecondModal() {
     this.secondModalOpenForm = false;
   }
-
   onSubmit() {
-    console.log('First Modal Confirmed');
     this.modalOpenForm = false;
     this.openSecondModal();
   }
 
   openSecondModal() {
-    console.log('Opening Second Modal');
     this.secondModalOpenForm = true;
   }
 
   onSecondModalSubmit() {
-    console.log('Second Modal Confirmed');
     this.secondModalOpenForm = false;
     // Additional logic for the second modal's confirm action can be added here
   }
